3717e3672730d0a51129e8283e70fa507589ae69
cat dev <bigcatchonker@mail.ru>
2024-06-11 21:18:34 +0300

    v0.0.7b hotfix

--- maze_game/run.py
+++ maze_game/run.py
@@ -1,13 +1,12 @@
 from requests import exceptions
-from dotenv import load_dotenv
 from datetime import datetime
 from bot import data_api
 from bot.bot import bot
-import sys
-import os
+from sys import stdout
+from sys import argv
+from os import execv
 
 if __name__ == "__main__":
-    load_dotenv()
     print(f"[{datetime.now().strftime("%H:%M:%S")}] Attempting to start data API...")
     data_api.start_api()
     print(f"[{datetime.now().strftime("%H:%M:%S")}] Data API started.")
@@ -16,7 +15,7 @@ if __name__ == "__main__":
         bot.infinity_polling(timeout=10, long_polling_timeout=5)
     except (exceptions.ConnectionError, exceptions.ReadTimeout) as e:
         print(f"[{datetime.now().strftime("%H:%M:%S")}] Caught exception {e}. Restarting the bot...")
-        sys.stdout.flush()
-        os.execv(sys.argv[0], sys.argv)
+        stdout.flush()
+        execv(argv[0], argv)
     else:
         bot.infinity_polling(timeout=10, long_polling_timeout=5)
